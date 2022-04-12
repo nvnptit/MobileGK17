d98a8d55fd691c309d7af1fe13639836e35efaf5
nvnptit <nvnptit@gmail.com>
2022-04-12 09:39:44 +0700

    update bottom nav

--- app/src/main/java/com/nvn/mobilegk17/MainActivity.java
+++ app/src/main/java/com/nvn/mobilegk17/MainActivity.java
@@ -1,14 +1,59 @@
 package com.nvn.mobilegk17;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    QuanLySanPhamFragment quanLySanPhamFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setControl();
+        setEvent();
     }
+
+    private void setControl() {
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        quanLySanPhamFragment = new QuanLySanPhamFragment();
+    }
+
+    private void setEvent() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_sanpham) {
+                    setFragment(quanLySanPhamFragment);
+                    return true;
+                } else if (id == R.id.nav_congnhan) {
+                 //   setFragment(CongNhanFragment);
+                    return true;
+                } else if (id == R.id.nav_chamcong) {
+                 //   setFragment(ChamCongFragment);
+                    return true;
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.nav_sanpham);
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_container, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
---
+++ app/src/main/java/com/nvn/mobilegk17/QuanLySanPhamFragment.java
@@ -0,0 +1,29 @@
+package com.nvn.mobilegk17;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class QuanLySanPhamFragment extends Fragment {
+
+    public QuanLySanPhamFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quan_ly_san_pham, container, false);
+    }
+}
