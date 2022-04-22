1b0941a5ee24c10184b0d863b6ed3cad817e0e81
nvnptit <nvnptit@gmail.com>
2022-04-22 16:50:51 +0700

    update Cong Nhan, Cham Cong, Bieu Do

--- app/src/main/java/com/nvn/mobilegk17/fragment/BieuDoFragment.java
+++ app/src/main/java/com/nvn/mobilegk17/fragment/BieuDoFragment.java
@@ -1,7 +1,11 @@
 package com.nvn.mobilegk17.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,9 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.nvn.mobilegk17.R;
+import com.nvn.mobilegk17.activity.ChartActivity;
 
 public class BieuDoFragment extends Fragment {
-
+    CardView bdSanPham,bdCongNhan,bdChamCong;
     public BieuDoFragment() {
         // Required empty public constructor
     }
@@ -24,7 +29,41 @@ public class BieuDoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bieu_do, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setControl();
+        setEvent();
+    }
+
+    private void setEvent() {
+        bdSanPham.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), ChartActivity.class);
+                startActivity(intent);
+            }
+        });
+        bdCongNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        bdChamCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void setControl() {
+        bdSanPham = getActivity().findViewById(R.id.card1);
+        bdCongNhan = getActivity().findViewById(R.id.card2);
+        bdChamCong = getActivity().findViewById(R.id.card3);
     }
 }
